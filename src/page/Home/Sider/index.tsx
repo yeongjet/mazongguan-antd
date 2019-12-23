0442ad954246ceade3abfd9e585b9e281b5cf333
yeongjet <625436194@163.com>
2019-12-24 00:23:45 +0800

    sider的正确使用方式

--- src/page/Home/Sider/index.tsx
+++ src/page/Home/Sider/index.tsx
@@ -9,6 +9,11 @@ const Body = styled(Antd.Layout.Sider).attrs({
     theme: 'light',
     collapsible: true
 })`
+    overflow-y: scroll;
+    overflow-x: hidden;
+    ::-webkit-scrollbar {
+        display: none;
+    }
     height: 100%;
     background: #ffffff;
 `
@@ -24,7 +29,7 @@ const Menu = styled(Antd.Menu).attrs({
 
 const Frame = styled.section`
     height: 100%;
-    min-width: 200px;
+    /* min-width: 200px; */
     overflow-y: scroll;
     overflow-x: hidden;
     ::-webkit-scrollbar {
@@ -42,75 +47,73 @@ export const Sider = () => {
         setCollapsed(!collapsed)
     }
     return (
-        <Frame>
-            <Body onCollapse={toggleCollapsed}>
-                <Menu>
-                    <SubMenu
-                        key="sub-menu-1"
-                        title={
-                            <span>
-                                <Icon type="area-chart" />
-                                <span>数据魔盒</span>
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="menu-item-1">数据概览</Menu.Item>
-                        <Menu.Item key="menu-item-2">活动数据</Menu.Item>
-                    </SubMenu>
-                    <SubMenu
-                        key="sub-menu-2"
-                        title={
-                            <span>
-                                <Icon type="barcode" />
-                                <span>一物一码</span>
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="menu-item-3">商品管理</Menu.Item>
-                        <Menu.Item key="menu-item-4">赋码管理</Menu.Item>
-                    </SubMenu>
-                    <Item key="menu-item-5">
+        <Body onCollapse={toggleCollapsed}>
+            <Menu>
+                <SubMenu
+                    key="sub-menu-1"
+                    title={
                         <span>
-                            <Icon type="profile" />
-                            <span>订单管理</span>
+                            <Icon type="area-chart" />
+                            <span>数据魔盒</span>
                         </span>
-                    </Item>
-                    <SubMenu
-                        key="sub-menu-4"
-                        title={
-                            <span>
-                                <Icon type="project" />
-                                <span>活动管理</span>
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="menu-item-6">活动总览</Menu.Item>
-                        <Menu.Item key="menu-item-7">扫码领红包</Menu.Item>
-                        <Menu.Item key="menu-item-8">幸运大转盘</Menu.Item>
-                        <Menu.Item key="menu-item-9">扫码排行榜</Menu.Item>
-                        <Menu.Item key="menu-item-10">门店销售</Menu.Item>
-                    </SubMenu>
-                    <Item key="menu-item-11">
+                    }
+                >
+                    <Menu.Item key="menu-item-1">数据概览</Menu.Item>
+                    <Menu.Item key="menu-item-2">活动数据</Menu.Item>
+                </SubMenu>
+                <SubMenu
+                    key="sub-menu-2"
+                    title={
                         <span>
-                            <Icon type="solution" />
-                            <span>渠道管理</span>
+                            <Icon type="barcode" />
+                            <span>一物一码</span>
                         </span>
-                    </Item>
-                    <SubMenu
-                        key="sub-menu-5"
-                        title={
-                            <span>
-                                <Icon type="layout" />
-                                <span>页面管理</span>
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="menu-item-12">图文页面</Menu.Item>
-                        <Menu.Item key="menu-item-13">小程序首页</Menu.Item>
-                    </SubMenu>
-                    <PlaceHolder></PlaceHolder>
-                </Menu>
-            </Body>
-        </Frame>
+                    }
+                >
+                    <Menu.Item key="menu-item-3">商品管理</Menu.Item>
+                    <Menu.Item key="menu-item-4">赋码管理</Menu.Item>
+                </SubMenu>
+                <Item key="menu-item-5">
+                    <span>
+                        <Icon type="profile" />
+                        <span>订单管理</span>
+                    </span>
+                </Item>
+                <SubMenu
+                    key="sub-menu-4"
+                    title={
+                        <span>
+                            <Icon type="project" />
+                            <span>活动管理</span>
+                        </span>
+                    }
+                >
+                    <Menu.Item key="menu-item-6">活动总览</Menu.Item>
+                    <Menu.Item key="menu-item-7">扫码领红包</Menu.Item>
+                    <Menu.Item key="menu-item-8">幸运大转盘</Menu.Item>
+                    <Menu.Item key="menu-item-9">扫码排行榜</Menu.Item>
+                    <Menu.Item key="menu-item-10">门店销售</Menu.Item>
+                </SubMenu>
+                <Item key="menu-item-11">
+                    <span>
+                        <Icon type="solution" />
+                        <span>渠道管理</span>
+                    </span>
+                </Item>
+                <SubMenu
+                    key="sub-menu-5"
+                    title={
+                        <span>
+                            <Icon type="layout" />
+                            <span>页面管理</span>
+                        </span>
+                    }
+                >
+                    <Menu.Item key="menu-item-12">图文页面</Menu.Item>
+                    <Menu.Item key="menu-item-13">小程序首页</Menu.Item>
+                </SubMenu>
+                <PlaceHolder></PlaceHolder>
+            </Menu>
+        </Body>
     )
 }
